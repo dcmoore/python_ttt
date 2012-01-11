3d62435f72d2eb885dfe228050fe8e048d0e03d5
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2012-01-11 15:27:13 -0600

    Minor fix to max_depth feature.

--- src/computer_player.py
+++ src/computer_player.py
@@ -2,7 +2,7 @@ class ComputerPlayer():
   def get_best_move(self, board, rules):
     max_depth = 6
     def _minimax(current_depth, minimax_vals_for_depth):
-      if current_depth == max_depth: return 0
+      if current_depth >= max_depth: return 0
       if rules.is_game_over(board):
         if rules.get_winner(board) == None:
           return 0
